9c938df3b23e1065c672fce2513b1196a740512c
olzhas2102 <olzhas2102@yahoo.com>
2018-04-27 18:34:07 +0200

    some styling

--- src/components/news_list.js
+++ src/components/news_list.js
@@ -4,7 +4,9 @@ import NewsItem from './news_list_item';
 const NewsList = (props) => {
   const items = props.news.map(item => {
     return (
-      <NewsItem key={item.id} item={item}/>
+      <NewsItem key={item.id} item={item}>
+        The News are:
+      </NewsItem>
     );
   });
   
